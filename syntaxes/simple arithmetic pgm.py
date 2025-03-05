3f948b4df5f5cbff187f426ff241441557e2e711
DeepikaVijayan <158603087+Deepikavijayan2000@users.noreply.github.com>
2025-03-05 15:17:59 +0530

    Create simple arithmetic pgm.py

---
+++ syntaxes/simple arithmetic pgm.py
@@ -0,0 +1,10 @@
+### This is a basic program which reads 2 numbers from user and execute basic arithmetic operations
+
+
+num1 = int(input("Entre the first number"))
+num2 = int(input("Entre the second number"))
+add = num1 + num2
+sub = num1 - num2
+mul = num1 * num2
+divi = num1/ num2
+print(f"\n {add} \n {sub} \n {mul}\n {divi}")
