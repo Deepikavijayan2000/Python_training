e70907356f72dc7ebb6e257d2cd2eacf8c467de5
DeepikaVijayan <158603087+Deepikavijayan2000@users.noreply.github.com>
2025-03-05 15:24:05 +0530

    Update and rename a to if_loop.py

---
+++ syntaxes/if_loop.py
@@ -0,0 +1,19 @@
+###SYNTAX FOR IF LOOP
+'''if condition
+      statments
+   else   
+      statements 
+      '''
+x = 10
+
+if x > 0:
+    print("x is positive")
+else:
+    print("x is zero")
+
+name = "Alice"
+
+if name == "Alice":
+    print("Hello, Alice!")
+else:
+    print("You are not Alice.")
