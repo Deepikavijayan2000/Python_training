42bd37dfa882719ea95a10bbcff18676babda825
DeepikaVijayan <158603087+Deepikavijayan2000@users.noreply.github.com>
2025-03-05 15:20:31 +0530

    Update simple arithmetic pgm.py

--- syntaxes/simple arithmetic pgm.py
+++ syntaxes/simple arithmetic pgm.py
@@ -1,5 +1,5 @@
 ### This is a basic program which reads 2 numbers from user and execute basic arithmetic operations
-
+### "input() " is the keyword we use to take a input from user
 
 num1 = int(input("Entre the first number"))
 num2 = int(input("Entre the second number"))
