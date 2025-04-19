d58c846f1310e05c2c43296f913114ca2b71917c
DeepikaVijayan <158603087+Deepikavijayan2000@users.noreply.github.com>
2025-04-19 12:53:46 +0530

    Add files via upload

---
+++ syntaxes/insertion_sort.py
@@ -0,0 +1,13 @@
+A = [-5,2,3,1,-3,-3,4,7,-4]
+
+def insertion_sort(arr):
+    n=len(arr)
+    for i in range(1, n):
+        for j in range(i, 0, -1):
+            if arr[j-1] > arr[j]:
+                arr[j-1], arr[j] = arr[j], arr[j-1]
+            else:
+                break
+
+insertion_sort(A)
+print(A)
